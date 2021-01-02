e8870f855deb7ee90e2436a78e1e1815eaf41b12
Julio César <julio_cesar.jcr@hotmail.com>
2021-01-02 15:34:25 -0300

    Adicionando páginas dinâmicas com NJK

---
+++ desafio_mod2/backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+
+const server = express();
+
+server.use(express.static("public"));
+
+server.set("view engine", "njk");
+
+server.get("/", function(req, res){
+    return res.render("about");
+});
+
+server.get("/content", function(req, res){
+    return res.render("content");
+});
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.listen(5000, function(){
+    console.log('server running');
+});
