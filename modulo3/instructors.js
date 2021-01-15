e62944ee43189aef8690a9cdefcf759ee0c98f35
Julio César <julio_cesar.jcr@hotmail.com>
2021-01-14 21:24:33 -0300

    Separando funções e rotas

---
+++ modulo3/instructors.js
@@ -0,0 +1,13 @@
+// função create 
+exports.post = function(req, res){
+
+    const keys = Object.keys(req.body)
+
+    for(key of keys){
+        if(req.body[key] == ""){
+            return res.send('Fill all the fields!')
+        }
+    }
+
+    return res.send(req.body)
+}
